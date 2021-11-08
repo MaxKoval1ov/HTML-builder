1c808ecac2e9cbfa32b55a253f1167f70b5f139d
MaxKoval1ov <maxkov112211@gmail.com>
2021-11-08 10:31:24 +0300

    first and second are done

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const process = require('process')
+
+let writeableStream = fs.createWriteStream("./text.txt",{
+    flags: 'a+' // 'a' means appending (old data will be preserved)
+  });
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+    })
+
+readline.on("line", (msg) => {
+    if(msg == "exit")
+    {
+      process.exit();
+    }
+    writeableStream.write(msg + "\n");
+})
+  
+process.on('SIGINT', (signal) => {
+    console.log(`Received ${signal}`);
+    process.exit();
+})
+
+process.on('exit', (code) => {
+    readline.close();
+    writeableStream.end();
+    console.log('Bye bye!Process exit event with code: ', code);
+  });
+console.log("Hello, write smth to file:");
